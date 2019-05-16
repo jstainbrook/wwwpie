4412deca99d552259103c94b6ab9fe2ba8f569cc
jstainbrook <jstainbrook@github.com>
2019-05-16 04:35:02 -0400

    revert

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -5,42 +5,19 @@
     <title>WWWPie</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
     <!-- JS -->
-    <script
-         src="https://code.jquery.com/jquery-3.4.1.min.js"
-         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-         crossorigin="anonymous"></script>   
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
 
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+    <!-- CSS -->
+    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-  <a class="navbar-brand" href="#">LOGO</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span class='fa fa-home'></span> Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><span class='fa fa-envelope'></span> </span>Messages <span class='badge badge-info' id='badge'>0</span></span></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0" id="searchform">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="searchbox"><span class="fa fa-search search"></span>  
-    </form>
-  </div>
-</nav>
-<div class="container">
+    <!-- logo -->
+    <div class="logo">
+        WWWPie - In development
+    </div>
 
     <!-- navigation -->
     <div class="navigation">
@@ -49,4 +26,3 @@
         <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
         <a href="<?php echo URL; ?>songs">songs</a>
     </div>
-</div>
