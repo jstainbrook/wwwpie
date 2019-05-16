f0f36a43a5e388a41fc50bea1456e84fdb30dfe8
jstainbrook <jstainbrook@github.com>
2019-05-16 04:30:54 -0400

    jquery addition

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -5,6 +5,8 @@
     <title>WWWPie</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
     <!-- JS -->
     <script
@@ -13,21 +15,15 @@
          crossorigin="anonymous"></script>   
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
 
-    <!-- CSS -->
-    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 </head>
 <body>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
   <a class="navbar-brand" href="#">LOGO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-<!-- font awesome -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -45,13 +41,12 @@
   </div>
 </nav>
 <div class="container">
-	<div class="row">
-		<h2>Auto-refreshing badge numbers on navbar</h2>
-		
-	</div>
-	<div class="row">
-	    Just a simple little bootsnipp with jquery to auto-update notification badge.  You can combine this with ajax to get a count from a database every thirty seconds or so, i.e. facebook style.  For the purposes of this bootsnipp, I'm initializing with 0 and getting a random number between 1-100 from random.org to illustrate.
-	    <br><br>Really, you would use this with an SQL count check, i.e.: <code class='p-2'>select count(*) as count from table where column='criteria'</code>Make a function in whatever back-end language you are using (php, javascript, asp, whatever) then call that function with ajax, returning only a number to the page.  So, if you are recording messages in the database, you can get an accurate count every whatever-interval-you-set.  For the purposes of this demo I've got it set to 5 seconds.
-	    <br><br>Also in this snipp, a little css here with search on nav to include faded search icon from font awesome.
-	</div>
-</div>
+
+    <!-- navigation -->
+    <div class="navigation">
+        <a href="<?php echo URL; ?>">home</a>
+        <a href="<?php echo URL; ?>home/exampleone">subpage</a>
+        <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
+        <a href="<?php echo URL; ?>songs">songs</a>
+    </div>
+</div<
