18513f66b2f28864ac6c695f6fdce04badb474a2
jstainbrook <jstainbrook@github.com>
2019-05-16 04:26:06 -0400

    jquery addition

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -20,11 +20,14 @@
 
 </head>
 <body>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
   <a class="navbar-brand" href="#">LOGO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+<!-- font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -42,12 +45,13 @@
   </div>
 </nav>
 <div class="container">
-
-    <!-- navigation -->
-    <div class="navigation">
-        <a href="<?php echo URL; ?>">home</a>
-        <a href="<?php echo URL; ?>home/exampleone">subpage</a>
-        <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
-        <a href="<?php echo URL; ?>songs">songs</a>
-    </div>
-</div<
+	<div class="row">
+		<h2>Auto-refreshing badge numbers on navbar</h2>
+		
+	</div>
+	<div class="row">
+	    Just a simple little bootsnipp with jquery to auto-update notification badge.  You can combine this with ajax to get a count from a database every thirty seconds or so, i.e. facebook style.  For the purposes of this bootsnipp, I'm initializing with 0 and getting a random number between 1-100 from random.org to illustrate.
+	    <br><br>Really, you would use this with an SQL count check, i.e.: <code class='p-2'>select count(*) as count from table where column='criteria'</code>Make a function in whatever back-end language you are using (php, javascript, asp, whatever) then call that function with ajax, returning only a number to the page.  So, if you are recording messages in the database, you can get an accurate count every whatever-interval-you-set.  For the purposes of this demo I've got it set to 5 seconds.
+	    <br><br>Also in this snipp, a little css here with search on nav to include faded search icon from font awesome.
+	</div>
+</div>
